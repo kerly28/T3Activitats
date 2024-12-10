3ce807474b7bf0ff5244e6f42a91887910000a67
kerlu <kerly.guanoquiza.7e8@itb.cat>
2024-12-10 23:52:15 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,24 +6,7 @@ namespace T3Activitats
     {
         public static void Main()
         {
-            // gatito
-            Cat myCat = new Cat("Luchy", 3, "Snowshoe", "Salmon");
-
-            // Informacio Luchy
-            Console.WriteLine("Cat Info:");
-            Console.WriteLine("Name: " + myCat.GetName());
-            Console.WriteLine("Age: " + myCat.GetAge());
-            Console.WriteLine("Breed: " + myCat.GetBreed());
-            Console.WriteLine("Favorite Food: " + myCat.GetFavoriteFood());
-
-            // Accioons luchy
-            Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Console.WriteLine("\nActions:");
-            myCat.Meow();
-            myCat.Play();
-            myCat.Wash();
-            myCat.Eat();
-            myCat.Hunt();
+        
         }
     }
 }
