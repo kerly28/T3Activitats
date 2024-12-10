5a506367d3e7d59cd4aa12137c12f952e0f5e1ca
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-12-10 13:58:55 +0100

    Closes #1 - exercici 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+
+namespace T3Activitats
+{
+    public class Program
+    {
+        public static void Main()
+        {
+          
+            Player player1 = new Player("Adriana", 23);
+            Player player2 = new Player("Anna", 23);
+            Player player3 = new Player("Paula", 22);
+            //numero de jugadors
+            Player[] teamPlayers = new Player[] { player1, player2, player3 };
+            //nom del equip
+            Team team = new Team("Las Sirenas", teamPlayers);
+            //mostrar equip
+            Console.WriteLine("Team: " + team.GetTeamName());
+            Console.WriteLine("Players:");
+            //mostrar jugadors (nom i edat)
+            foreach (var player in team.GetPlayers())
+            {
+                Console.WriteLine($"Name: {player.GetName()}, Age: {player.GetAge()}");
+            }
+        }
+    }
+}
