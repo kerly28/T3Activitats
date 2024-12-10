330b512f19744c93a9abf2f64971d53a7c339c05
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-12-10 23:05:17 +0100

    Closes #14 - exercici 7

--- Program.cs
+++ Program.cs
@@ -10,26 +10,26 @@ namespace T3Activitats
            
                 Bike bike1 = new Bike("Mountain", "Red", 20); 
                 Console.WriteLine("Bike 1:\n -Type = " + bike1.GetType() + "\n -Color = " + bike1.GetColor() + "\n -Speed = " + bike1.GetSpeed());
-                Console.WriteLine("-------------------------------------------------");
+                Console.WriteLine("--------------------------------------------------");
 
             // Bike 2
             try
             {
                 Bike bike2 = new Bike("", "Blue", 15); 
                 Console.WriteLine("Bike 2: \n -Type = " + bike2.GetType() + " \n -Color = " + bike2.GetColor() + "\n -Speed = " + bike2.GetSpeed());
-                Console.WriteLine("-------------------------------------------------");
+                Console.WriteLine("--------------------------------------------------");
             }
             catch (ArgumentException ex)
             {
                 Console.WriteLine("Error en Bike 2: " + ex.Message);
-                Console.WriteLine("-------------------------------------------------");
+                Console.WriteLine("---------------------------------------------------");
             }
 
             //Bike 3
            
                 Bike bike3 = new Bike("Road", "Green", -5); 
                 Console.WriteLine("Bike 3: \n -Type = " + bike3.GetType() + "\n -Color = " + bike3.GetColor() + "\n -Speed = " + bike3.GetSpeed());
-                Console.WriteLine("-------------------------------------------------");       
+                Console.WriteLine("--------------------------------------------------");       
         }
     }
 }
